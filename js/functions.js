74d0f15dfca405f738fd720078abaf2da8f637ac
Rafael <rafaelrodrigues_@outlook.com>
2020-04-02 17:28:17 -0300

    Paginação na área de respostas

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,11 @@
 // Arquivo JS do projeto
+
+// PLUGIN JPAGES -- RESPONSE__MANAGEMENT
+$(function(){
+
+    $("div.holder").jPages({
+      containerID : "management",
+      perPage : 10,
+    });
+  
+  });
