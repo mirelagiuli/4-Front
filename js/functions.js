91c30cfcfc3a482a5f2f3e3654da79a37185d0eb
Pedro <phrbrock@gmail.com>
2020-04-03 11:45:58 -0300

    Adicionado codigo para valição de login

--- js/functions.js
+++ js/functions.js
@@ -9,3 +9,25 @@ $(function(){
     });
   
   });
+
+/*
+ *Codigo para validação do login 
+*/
+
+function validaLogin(){
+    
+  const username = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  
+  if ( username == "academy@cocreare.com.br" && password == "123456"){        
+      window.location = "/management.html";
+      return true;
+  } else {
+      alert ("Login ou senha incorreta!!");
+      return false;
+  }
+}
+
+/**
+ * Fim da validação de login
+ */
